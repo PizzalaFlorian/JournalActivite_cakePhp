d6072f7d214224d3184c325b5162dc07a408bd5f
PizzalaFlorian <florian.pizzala@gmail.com>
2016-05-09 09:39:15 +0200

    migration step 1.5

--- vendor/functionperso/activite/categorie.activite.php
+++ vendor/functionperso/activite/categorie.activite.php
@@ -3,36 +3,71 @@
 		use Cake\ORM\TableRegistry;
 		/* Renvoie un tableau d'objet php contenant la liste complete des CategorieActivités de la BDD*/
 		function get_CategorieActivite($bdd){
-			$requete = "SELECT * FROM categorieactivite ";
-			$sth = $bdd->prepare($requete);
-			$sth->execute();
+			// $requete = "SELECT * FROM categorieactivite ";
+			// $sth = $bdd->prepare($requete);
+			// $sth->execute();
 			
-			while ($reponse = $sth->fetch(PDO::FETCH_ASSOC))
-			{
-				$listeCategorieActivite[] = new categorieActivite($reponse);
-			}
-			return $listeCategorieActivite;
+			// while ($reponse = $sth->fetch(PDO::FETCH_ASSOC))
+			// {
+			// 	$listeCategorieActivite[] = new categorieActivite($reponse);
+			// }
+			// return $listeCategorieActivite;
+
+			$table = null;
+			$table = TableRegistry::get('categorieactivite')
+		    	->find()
+		    	->toArray();
 		}
 	
 		/* Renvoie le tableau de la liste des catégories d'activités en html*/
 		function tableauCategorie($bdd){
-			$requete = $bdd->query("SELECT * FROM categorieactivite");
+			// $requete = $bdd->query("SELECT * FROM categorieactivite");
+			// $resultat = "<table>\n<tr><th>Nom de la catégorie</th></tr>\n";
+			// while ($data = $requete->fetch()){
+			// 	$resultat.="<tr><td>".$data['NomCategorie']."</td></tr>\n";
+			// }
+			// $resultat.="</table>";
+			// return $resultat;
+
 			$resultat = "<table>\n<tr><th>Nom de la catégorie</th></tr>\n";
-			while ($data = $requete->fetch()){
-				$resultat.="<tr><td>".$data['NomCategorie']."</td></tr>\n";
-			}
-			$resultat.="</table>";
+
+			$table = null;
+			$table = TableRegistry::get('categorieactivite')
+		    	->find()
+		    	->toArray();
+
+		    foreach($table as $data){
+		    	$resultat.="<tr><td>".$data['NomCategorie']."</td></tr>\n";
+		    }
+		    
+		    $resultat.="</table>";
+			
 			return $resultat;
 		}
 		
 		/* Renvoie un select avec la liste des catégories d'activités */
 		function selectCategorie($bdd,$id,$name){
-			$resultat = "<select name=\"$name\" id=\"$id\">\n";
-			$requete = $bdd->query("SELECT * FROM categorieactivite");
-			while ($data = $requete->fetch()){
-				$resultat.="\t<option value=\"".$data['CodeCategorieActivite']."\">".$data['NomCategorie']."</option>\n";
-			}
-			$resultat.="</select>";
+			// $resultat = "<select name=\"$name\" id=\"$id\">\n";
+			// $requete = $bdd->query("SELECT * FROM categorieactivite");
+			// while ($data = $requete->fetch()){
+			// 	$resultat.="\t<option value=\"".$data['CodeCategorieActivite']."\">".$data['NomCategorie']."</option>\n";
+			// }
+			// $resultat.="</select>";
+			// return $resultat;
+
+			$resultat = "<table>\n<tr><th>Nom de la catégorie</th></tr>\n";
+
+			$table = null;
+			$table = TableRegistry::get('categorieactivite')
+		    	->find()
+		    	->toArray();
+
+		    foreach($table as $data){
+		    	$resultat.="<tr><td>".$data['NomCategorie']."</td></tr>\n";
+		    }
+		    
+		    $resultat.="</table>";
+			
 			return $resultat;
 		}
 		
