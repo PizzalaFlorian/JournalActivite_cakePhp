e7de122151388fd6fcbdabd0b3242f5579f3e87e
csomorb <csomorbarnabas1@gmail.com>
2016-06-08 12:44:56 +0000

    mimification 1

--- vendor/cakephp-plugins.php
+++ vendor/cakephp-plugins.php
@@ -2,9 +2,11 @@
 $baseDir = dirname(dirname(__FILE__));
 return [
     'plugins' => [
+        'AssetCompress' => $baseDir . '/vendor/markstory/asset_compress/',
         'Bake' => $baseDir . '/vendor/cakephp/bake/',
         'Cake/Localized' => $baseDir . '/vendor/cakephp/localized/',
         'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
-        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'MiniAsset' => $baseDir . '/vendor/markstory/mini-asset/'
     ]
 ];
